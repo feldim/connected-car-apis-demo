546c3fb1fe010720399f5928da817464ba379b8d
Felix <fd033@hdm-stuttgart.de>
2020-04-28 11:40:58 +0200

    basic nodeJS setup

--- cc-apis-react-demo/src/components/OtonomoApi/OtonomoApi.js
+++ cc-apis-react-demo/src/components/OtonomoApi/OtonomoApi.js
@@ -2,11 +2,42 @@ import React from 'react';
 //import PropTypes from 'prop-types';
 import styles from './OtonomoApi.module.scss';
 
-const OtonomoApi = () => (
-  <div className={styles.OtonomoApi} data-testid="OtonomoApi">
-    OtonomoApi Component
-  </div>
-);
+class OtonomoApi extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={apiResponse:""}; //set response with empty value
+
+  }
+
+  callAPI(){
+    console.log("api call")
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentDidMount(){
+    this.callAPI();
+  }
+
+  //react render function
+  render(){
+    return (
+      <div className={styles.OtonomoApi} data-testid="OtonomoApi">
+      OtonomoApi Component
+      <p>Test:{this.state.apiResponse}</p>
+    </div>
+    );
+  }
+
+}
+
+
+
+
+
+
+
 
 OtonomoApi.propTypes = {};
 
